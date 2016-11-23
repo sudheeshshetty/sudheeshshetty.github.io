89a14600952e393806fe90ef5d2af31c17f04995
Sudheesh Shetty <sudheeshshetty@gmail.com>
2016-11-23 23:19:15 +0530

    New design

---
+++ js/temp.js
@@ -0,0 +1,11 @@
+/*(function($){
+    $(function(){
+
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space*/
+$(document).ready(function(){
+    $('.parallax').parallax();
+});
